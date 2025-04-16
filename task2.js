897181cabf9a5fce7e135da6749842f725a35270
Unique-Ikeoluwa <ikeoluwaadegbite@gmail.com>
2025-04-16 16:15:05 +0100

    final task2 assignment

--- task2.js
+++ task2.js
@@ -16,31 +16,8 @@ libraryBooks("Last days at Forcados high school", 2012)
 libraryBooks("The ugly ones refuse to die", 2005)
 libraryBooks("Half of a yellow sun", 2010)
 libraryBooks("Magic pot", 2006)
-libraryBooks("Indepence", 2016)
+libraryBooks("Independence", 2016)
 // console.log(library)
-
-// or 
-
-// let book1 = {
-//     title: "Mirror", 
-//     year: 2025
-// }
-// let book2 = {
-//     title: "Last days at Forcados high school", 
-//     year: 2012
-// }
-// let book3 = {
-//     title: "The ugly ones refuse to die", 
-//     year: 2005
-// }
-// let book4 = {
-//     title: "Half of a yellow sun", 
-//     year: 2010
-// }
-// let library = [book1, book2, book3, book4]
-// console.log(library)
-
-// Delete a book from the library
 
 function deleteBook(book){
     for (let i = 0; i < library.length; i++){
@@ -65,21 +42,17 @@ function updateBook(bookTitle){
 let updatedBook = updateBook("Half of a yellow sun")
 console.log(`the year of publication for ${updatedBook.title} is ${updatedBook.year}`)
 
+// Get a book from the library
 
-// let bookValues = Object.values(library)
-// console.log(bookValues)
-// if (bookValues.title === "Half of a yellow sun"){
-//     bookValues.year = 2006
-//     return book
-// }
-// console.log(bookValues)
+function getBook(bookNeed){
+    for (let i = 0; i < library.length; i++){
+        if (library[i].title === bookNeed){
+            return library[i]
+        }
+    }
+}
+let bookNeeded = getBook("Independence")
+console.log(bookNeeded)
 
-// What is wrong with my code?   function updateBook(bookTitle){
-//     for (book of library){
-//         if (book.title === bookTitle){
-//             book.year === 2006
-//         }
-//     }
-// }
-// updateBook("Half of a yellow sun")
-// console.log(`the year of publication for ${book.title} is ${book.year}`)
+// View all the books in the library
+console.log(library)
